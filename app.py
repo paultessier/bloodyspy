311110495509074058b9a4437787bf988c88f437
paultessier <62594513+paultessier@users.noreply.github.com>
2022-07-18 18:17:14 +0200

    docker image fixed

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI, HTTPException, Request, File, UploadFile
+from fastapi import FastAPI, File, UploadFile
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from passlib.context import CryptContext
 from typing import List
+# from fastapi import Request
 # from pydantic import BaseModel
 # from typing import Optional
 import cv2
 import tensorflow as tf
-# from keras.preprocessing import image
 import numpy as np
-# import pandas as pd
+import re
+import os
 
 # interface graphique de l'API
 # http://localhost:8000/docs
@@ -21,54 +25,81 @@ import numpy as np
 #Your kernel may have been built without NUMA support
 
 
-api = FastAPI(
-    title="API - bloody spy",
-    description="This API aims to categorize blood cells",
-    version="1.0.1",
-    # port =8001
-)
+
 
 
 # ==== PARAMETERS ===================================================================
 
-# On crée la liste des noms des différents dossiers de types de cellules présents dans le dossier data_samples
-cell_types = ['neutrophil', 'eosinophil', 'ig', 'platelet', 'erythroblast', 'monocyte','basophil','lymphocyte']
-# On crée la liste des noms des différentes types de cellules que l'on souhaite afficher à l'avenir
-cell_types2 = ['neutrophil', 'eosinophil', 'immature granulocyte', 'platelet', 'erythroblast', 'monocyte','basophil','lymphocyte']
+# List of blood cell types
+cell_types = ['neutrophil', 'eosinophil', 'immature granulocyte', 'platelet', 'erythroblast', 'monocyte','basophil','lymphocyte']
+
+model_code2name={
+    'FS':'CNN',
+    'ResNet50':'TL ResNet50',
+    'VGG16':'TL VGG16',
+    'VGG19':'TL VGG19',
+    'Xception':'TL Xception'
+}
 
 
-# ==== API STATUS ===================================================================
 
-@api.get('/api/status', name='Verify status of the API')
-def get_status():
-    """Returns a success status if the API is working.
-    """
-    return {
-            "status": "success",
-            "message": "Hello from a public endpoint! You don't need to be authenticated to see this."
+# ==== MAIN ===================================================================
+
+api = FastAPI(
+    title="bloodyspy API",
+    description="This API classes images among {} blood cell types :\n{}"
+    .format(
+        len(cell_types),
+        '\n'.join(['  - '+c for c in cell_types])
+        ),
+    version="1.0.0"
+)
+
+security = HTTPBasic()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+users = {
+
+    "root" : {
+        "username" :  "root",
+        "name" : "Root user",
+        "hashed_password" : pwd_context.hash('root_password'),
+    },
+
+    "alice": {
+        "username": "alice",
+        "name": "Alice in wonderland",
+        "hashed_password": pwd_context.hash('wonderland'),
+    },
+
+    "paul" : {
+        "username" :  "paul",
+        "name" : "paul tessier",
+        "hashed_password" : pwd_context.hash('hello_pwd'),
     }
 
+}
 
-# ===== INFO ROUTES ===================================================================
+# class PATH(BaseModel):
+#     path: str
 
-# -------------------------------------------------------------------
-# 1/ Upload file
-# -------------------------------------------------------------------
 
-@api.post("/files/")
-async def create_file(file: bytes = File()):
-    return {"file_size": len(file)}
+# ==== FUNCTIONS ===================================================================
 
-@api.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
+def get_prediction(file, entrytype='file', model_code='VGG19'):
 
-    if not file:
-        return {"message": "No upload file sent"}
-    
-    extension = file.filename.split('.')[-1]
-    tmp_filename = f"tmp/tmp_img.{extension}"
-    with open(tmp_filename, "wb+") as file_object:
-        file_object.write(file.file.read())
+    if entrytype=='file':
+        extension = file.filename.split('.')[-1]
+        tmp_filename = f"tmp/tmp_img.{extension}"
+        with open(tmp_filename, "wb+") as file_object:
+            file_object.write(file.file.read())
+            file_object.close()
+
+    elif entrytype=='name':
+        tmp_filename = file
+
+    else:
+        return 'error: bad entry_type option. should be file or name'
 
     # On stocke l'image en RGB
     img_rgb = cv2.imread(tmp_filename,cv2.IMREAD_COLOR)
@@ -78,63 +109,151 @@ async def create_upload_file(file: UploadFile):
     img_f = cv2.resize(img_rgb,dsize = (60,60))
 
     # Normalization of pixels value
-    # img_tf = image.img_to_array(img_f)/255
     img_tf = tf.keras.utils.img_to_array(img_f)/255
-    print(img_tf.shape)
+    # print(img_tf.shape)
 
     #On ajoute une dimension à l'image
-    # img_tf = np.expand_dims(img_tf.tolist(), axis = 0)
-    # img_tf = np.expand_dims(img_tf, axis = 0)
     img_tf = np.expand_dims(img_tf, axis = 0)
-    print(img_tf.shape)
+    # print(img_tf.shape)
 
-    # On charge le modèle choisit par l'utilisateur
-    k=0
-    available_models=['FS','ResNet50','VGG16','VGG19','Xception']
-    model = tf.keras.models.load_model('./models/Save_model_CNN_'+available_models[k]+'_60x60_rgb.h5')
+    # we load the chosen model
+    model = tf.keras.models.load_model('./models/Save_model_CNN_'+model_code+'_60x60_rgb.h5')
 
-    # On stocke les valeurs de probabilité des classes de l'image
+    # probabilities for the image to be part of each class (=blood cell type)
     probas = model.predict(img_tf)[0]
-    probabilities = {c:p for c,p in zip(cell_types2,probas)}
-
-    # On stocke la classe retournée et la proba
-    pred_proba = np.max(probas)*100
-    pred_cell_type = cell_types2[np.argmax(probas)]
+    
+    # We store probabilities, and get the predicted blood cell type with its probability
+    # probabilities = {c:p*100 for c,p in zip(cell_types,probas)}
+    # pred_proba = np.max(probas)*100
+    probabilities = {c:round(p*100,2) for c,p in zip(cell_types,probas)}
+    pred_proba = round(np.max(probas)*100,2)
+    pred_cell_type = cell_types[np.argmax(probas)]
 
     return {
         "filename": file.filename,
-        "model":available_models[k],
+        "model":model_code2name[model_code],
         "predictions":probabilities,
-        "predicted_blood_cell_type":pred_cell_type,
-        "prediction_probability":pred_proba
+        "predicted":pred_cell_type,
+        "probability":pred_proba
     }
 
 
-@api.post("/uploadfile2/")
-async def create_upload_file(file: UploadFile):
-    # print(dir(file))
-    extension = file.filename.split('.')[-1]
-    tmp_filename = f"tmp/tmp_img.{extension}"
-    with open(tmp_filename, "wb+") as file_object:
-        file_object.write(file.file.read())
+def is_img(filename):
 
+    regex = "([^\\s]+(\\.(?i)(jpe?g|png|gif|bmp))$)" # Regex to check valid image file extension.
+    p = re.compile(regex) # Compile the ReGex
+    if re.search(p, filename):
+        return True
+    else:
+        return False
+
+
+def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    username = credentials.username
+    if not(users.get(username)) or not(pwd_context.verify(credentials.password, users[username]['hashed_password'])):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+# ===== ROUTES ===================================================================
+
+# -------------------------------------------------------------------
+# 1/ Status
+# -------------------------------------------------------------------
+
+@api.get('/bloodyspy/status', name='Verify status of the API')
+def get_status():
+    """Returns a status=1 if the API is working.
+    """
     return {
-        "filename": file.filename,
-        "content":file.content_type,
-        "file":file.file,
-        "headers":file.headers
+            "status": 1,
+            "message": "Hello from a public endpoint. You don't need to be authenticated to see this message."
     }
 
 
 # -------------------------------------------------------------------
-# 1/ Upload files
+# 2/ Upload image and get blood cell type
 # -------------------------------------------------------------------
 
-@api.post("/uploadfiles/")
-async def create_upload_files(
-    files: List[UploadFile] = File(description="Multiple files as UploadFile"),
+# @api.post("/files/")
+# async def create_file(file: bytes = File()):
+#     return {"file_size": len(file)}
+
+@api.post("/bloodyspy/image/", name='Get the cell type of the image uploaded')
+async def predict_img(
+    file: UploadFile,
+    username: str = Depends(get_current_user)
 ):
-    return {"filenames": [file.filename for file in files]}
+    """Returns basic information from the model, applied on a single image file.
+    """
+
+    if not file:
+        return {'username':username,'error': 'No file sent'}
+    
+    if is_img(file.filename):
+        print(file.filename)
+        result = get_prediction(file)
+        result['username']=username
+    else:
+        return {
+            'username':username,
+            'filename':file.filename,
+            'error':'not an image'
+        }
+    
+    return result
+
+
+# -------------------------------------------------------------------
+# 3/ Upload several images and get blood cell types
+# -------------------------------------------------------------------
+
+@api.post("/bloodyspy/images/", name='Get the cell type of several images uploaded')
+async def predict_imgs(
+    files: List[UploadFile] = File(description="Multiple files as UploadFile"),
+    username: str = Depends(get_current_user)
+):
+    """Returns basic information from the model, applied on several image files.
+    """
+
+    if not files:
+        return {'username':username,'error': 'No file sent'}
+
+    result=dict()
+    result['username']=username
+    for file in files:
+        if is_img(file.filename):
+            result[file.filename]=get_prediction(file)
+        else:
+            result[file.filename]={'filename':file.filename,'error':'not an image'}
+            
+    return result
+
+# -------------------------------------------------------------------
+# 3/ Browse a folder
+# -------------------------------------------------------------------
+
+# @api.post("/bloodyspy/path/")
+# async def predict_imgsInfolder(data : PATH):
+
+#     for root, _, files in os.walk(data.path):
+#         img_files=[]
+#         for file in files:
+#             if is_img(file):
+#                 img_files.append(os.path.join(root,file))
+
+#     print(img_files[:20])
+
+#     result=dict()
+#     for filename in img_files:
+#         print(filename)
+#         result[filename]=get_prediction(file,entrytype='name')
+
+#     return result
+
 
 
 
--- tests.sh
+++ tests.sh
@@ -1,24 +1,73 @@
-echo "================================================="
-echo TEST1: STATUS
-echo "================================================="
-curl -X GET -i 'http://127.0.0.1:8000/api/status'
+echo =====================================================================
+echo "The following base 64 conversion was done for authorization header:"
+# echo Authorization: Basic $(echo root:root_password | base64)
+echo Authorization: Basic $(printf '%s' "root:root_password" | base64)
+# echo Authorization: Basic $(echo alice:wonderland | base64)
+echo Authorization: Basic $(printf '%s' "alice:wonderland" | base64)
+# echo Authorization: Basic $(echo paul:hello_pwd | base64)
+echo Authorization: Basic $(printf '%s' "paul:hello_pwd" | base64)
+echo =====================================================================
 
 echo " "
-echo "================================================="
-echo TEST2: UPLOAD FILE
-echo "================================================="
+echo " "
+echo "===================================================================================="
+echo "   TEST 1: STATUS"
+echo "===================================================================================="
+curl -X GET -i 'http://localhost:8000/bloodyspy/status'
+
+echo " "
+echo " "
+echo "===================================================================================="
+echo "   TEST 2: GET CELL BLOOD TYPE FOR 1 IMAGE"
+echo "===================================================================================="
 curl -X 'POST' \
-  'http://localhost:8000/uploadfile/' \
+  'http://localhost:8000/bloodyspy/image/' \
   -H 'accept: application/json' \
+  -H 'Authorization: Basic cm9vdDpyb290X3Bhc3N3b3Jk' \
   -H 'Content-Type: multipart/form-data' \
   -F 'file=@data_unlabelled/10030044131_188.jpg;type=image/jpeg'
 
+
 echo " "
-echo "================================================="
-echo TEST3: FILE
-echo "================================================="
-  curl -X 'POST' \
-  'http://localhost:8000/files/' \
+echo " "
+echo "===================================================================================="
+echo "   TEST 2 BIS: SUBMIT A TEXT FILE -> ERROR EXPECTED"
+echo "===================================================================================="
+curl -X 'POST' \
+  'http://localhost:8000/bloodyspy/image/' \
   -H 'accept: application/json' \
+  -H 'Authorization: Basic cGF1bDpoZWxsb19wd2Q=' \
   -H 'Content-Type: multipart/form-data' \
-  -F 'file=@data_unlabelled/10028001812_206.jpg;type=image/jpeg'
+  -F 'file=@data_unlabelled/text_file.txt;type=text/plain'
+
+
+echo " "
+echo " "
+echo "===================================================================================="
+echo "   TEST 3: GET CELL BLOOD TYPE FOR SEVERAL IMAGES"
+echo "===================================================================================="
+curl -X 'POST' \
+  'http://localhost:8000/bloodyspy/images/' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Basic YWxpY2U6d29uZGVybGFuZA==' \
+  -H 'Content-Type: multipart/form-data' \
+  -F 'files=@data_unlabelled/10030044131_254.jpg;type=image/jpeg' \
+  -F 'files=@data_unlabelled/10028001812_147.jpg;type=image/jpeg' \
+  -F 'files=@data_unlabelled/10028001812_201.jpg;type=image/jpeg'
+
+
+echo " "
+echo " "
+echo "===================================================================================="
+echo "   TEST 3 BIS: SUBMIT SEVERAL FILES WHOSE 1 TEXT FILE -> ERROR EXPECTED"
+echo "===================================================================================="
+curl -X 'POST' \
+  'http://localhost:8000/bloodyspy/images/' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Basic YWxpY2U6d29uZGVybGFuZA==' \
+  -H 'Content-Type: multipart/form-data' \
+  -F 'files=@data_unlabelled/text_file.txt;type=text/plain' \
+  -F 'files=@data_unlabelled/monocyt.jpg;type=image/jpeg'
+
+echo " "
+echo " "
